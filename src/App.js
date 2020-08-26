938f3758140f081d9bde1f86e08c4100271ae251
Żaneta Jażdżyk <zaneta.jazdzyk@gmail.com>
2020-08-26 18:15:24 +0200

    step-1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <header>
+        <h1>Lista zadań</h1>
+      </header>
+      <main>
+        <ul>
+          <li>Todo #1</li>
+          <li>Todo #2</li>
+          <li>Todo #3</li>
+        </ul>
+      </main>
     </div>
   );
 }
